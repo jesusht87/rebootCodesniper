b32e7f59533479559a8ee44fc952c0e10ca8b5d7
Jesús <jesusht87@gmail.com>
2021-11-26 23:11:23 +0000

    next stage finished

--- script/main.js
+++ script/main.js
@@ -1,102 +1,9 @@
 var gameIsOn = false;
 
-const stages = [{
-    level: 1,
-    enemyRate: [1000, 5000], //1 new enemy between 1 and 5 seconds (random)
-    timer: 3000, //level timer is 1 minute
-    cleared: false,
-    class: 'level-one',
-    maplimits: [190, 320, 0, 475],
-    bgm: new Audio('media/sound/stage-music-1.mp3')
-},
-{
-    level: 2,
-    enemyRate: [500, 4500],
-    timer: 70000,
-    cleared: false,
-    class: 'level-two',
-    maplimits: [190, 320, 0, 475],
-    bgm: new Audio('media/sound/stage-music-2.mp3')
-},
-{
-    level: 3,
-    enemyRate: [500, 4500],
-    timer: 70000,
-    cleared: false,
-    class: 'level-two',
-    maplimits: [190, 320, 0, 475],
-    bgm: new Audio('media/sound/stage-music-3.mp3')
-}]
-
-function Stage(level) {
-    this.stage = stages.filter(e => { return e.level === level })[0]
-    this.timeDown = this.stage.timer;
-
-    this.timer = () => {
-        const countDown = document.getElementById('time')
-
-        if (document.getElementsByClassName('timer').length > 0) {
-            let currentTimer = document.querySelectorAll('.timer')
-            currentTimer.forEach(e => {
-                countDown.removeChild(e)
-            });
-        }
-
-        let counter = document.createElement('div')
-        counter.classList.add('timer')
-        counter.innerText = (this.timeDown / 1000)
-        countDown.appendChild(counter)
-
-    }
-
-    this.clear = () => {
-        //Show Game Clear, number of enemies killed and prepare next level.
-        // Checks if:
-        // 1. Stops music,attack & enemy timers OK
-        // 2. Creates new DIV showing the screen "continue-screen" OK
-        // 4. In that screen, when player clicks in "Continue"...
-        //      5. Level goes up to the next number (+1)
-        //      6. Enemies array will be cleared and will dissapear from DOM
-        //      7. Changes background 
-        //      8. We call to a new CodeSniper game
-        self.level.stage.bgm.pause()
-        clearInterval(this.enemyInterval)
-        this.enemyList.forEach(enemy => {
-            clearInterval(enemy.attackTimer)
-        });
-
-        const oldStage = document.getElementById('screen')
-        let newScreen = document.createElement('div')
-        newScreen.classList.add('stage')
-        newScreen.id = `continue-screen`
-        oldStage.appendChild(newScreen)
-
-        const stages = document.getElementById('stage')
-        stages.removeChild('div')  // Eliminar enemigos del DOM?
-
-        this.stage.cleared = true; 
-
-        window.addEventListener('click', e => {
-            if (e.target.getAttribute('id') === 'continue') {
-                this.newStage()
-            }
-        })
-        
-    }
-
-    this.screen = () => {
-        let screen = document.getElementById('stage')
-        screen.classList.add(this.stage.class)
-        let indicators = document.getElementById('indicators')
-        indicators.style.visibility = 'visible';
-    }
-}
-
 function CodeSniper() {
     const self = this;
-    this.stagelevel = 1; //Game starts on level 1.
     this.player = new Player();
-    this.level = new Stage(this.stagelevel);
+    this.level = new Stage();
     this.enemyList = []
 
     this.setCountdown = () => {
@@ -105,123 +12,100 @@ function CodeSniper() {
             self.level.timer()
             if (self.level.timeDown === 0) {
                 clearInterval(this.interval);
-                self.level.clear();
-                self.level.stage.bgm.pause();
-                clearInterval(this.enemyInterval);
+                this.stageClear()
             }
         }, 1000);
     }
 
-    this.enemyAppear = () => {
-        let enemyIntervalTimer = Math.floor(Math.random() * (this.level.stage.enemyRate[1] - this.level.stage.enemyRate[0] + 1) + this.level.stage.enemyRate[0]);
-        this.enemyInterval = setInterval(() => {
-
-            this.enemyList.push(new Enemy(this.stagelevel, this.player))
-            this.enemyList[this.enemyList.length - 1].create()
-            this.enemyAttack()
-
-            if (self.level.stage.cleared == true) {
-                clearInterval(this.enemyInterval)
-            } else {
-                clearInterval(this.enemyInterval)
-                self.enemyAppear();
+    this.stageClear = () => {
+        self.level.clear();
+        self.level.stage.bgm.pause();
+        clearInterval(this.enemyInterval);
+        document.getElementById('continue').addEventListener('click', () => {
+            if(this.level.stage.cleared == true){
+            this.level.newStage()
+            this.start()
             }
-        }, enemyIntervalTimer);
+        })
+        let parent = document.getElementById('stage')
+        let enemies = document.querySelectorAll('.enemy')
+        enemies.forEach(enemy => {
+            parent.removeChild(enemy)
+        });
+        this.enemyList.forEach(enemy => {
+            clearTimeout(enemy.attackTimer)
+        })
+        this.enemyList = []
     }
+
+    this.enemyAppear = () => {
+            let enemyIntervalTimer = Math.floor(Math.random() * (this.level.stage.enemyRate[1] - this.level.stage.enemyRate[0] + 1) + this.level.stage.enemyRate[0]);
+            this.enemyInterval = setInterval(() => {
+
+                this.enemyList.push(new Enemy(this.level.currentLevel, this.player))
+                this.enemyList[this.enemyList.length - 1].create()
+                this.enemyAttack()
+
+                if (self.level.stage.cleared == true) {
+                    clearInterval(this.enemyInterval)
+                } else {
+                    clearInterval(this.enemyInterval)
+                    self.enemyAppear();
+                }
+            }, enemyIntervalTimer);
+        }
 
     this.enemyAttack = () => {
-        let randCountdown = this.enemyList[this.enemyList.length - 1].timeOut
-        this.enemyList[this.enemyList.length - 1].attackTimer = setTimeout(this.enemyList[this.enemyList.length - 1].attack, randCountdown)
-    }
+            let randCountdown = this.enemyList[this.enemyList.length - 1].timeOut
+            this.enemyList[this.enemyList.length - 1].attackTimer = setTimeout(this.enemyList[this.enemyList.length - 1].attack, randCountdown)
+        }
 
     this.start = () => {
-        self.level.screen();
-        self.level.stage.bgm.play();
-        console.log(document.getElementsByTagName('audio')[0].remove())
+            self.level.screen();
+            self.level.stage.bgm.play();
+            document.getElementsByTagName('audio')[0].pause()
 
-        //Initial Lives
-        self.player.updateHP();
-        //Start Stage Timer CountDown
-        self.setCountdown();
-        //Load Weapon
-        self.player.reload()
-        //Create timer to introduce new enemies.
-        self.enemyAppear();
+            //Initial Lives
+            self.player.updateHP();
+            //Start Stage Timer CountDown
+            self.setCountdown();
+            //Load Weapon
+            self.player.reload()
+            //Create timer to introduce new enemies.
+            self.enemyAppear();
 
-        window.addEventListener('click', e => {
-            if (e.target.getAttribute('id') == 'stage' || e.target.getAttribute('class') == 'enemy') {
+            window.addEventListener('click', e => {
+                if (e.target.getAttribute('id') == 'stage' || e.target.getAttribute('class') == 'enemy') {
 
-                if (e.target.getAttribute('class') == 'enemy') {
-                    if (this.player.magazine > 0) {
-                        let parent = document.getElementById('stage')
-                        parent.removeChild(e.target)
-                        let targetIndex = this.enemyList.indexOf(this.enemyList.find(f =>
-                            f.enemyTag === e.target.getAttribute('id')
-                        ))
-                        clearTimeout(this.enemyList.find(f =>
-                            f.enemyTag === e.target.getAttribute('id')
-                        ).attackTimer)
-                        this.enemyList.splice(targetIndex, 1)
-                        console.log(this.enemyList)
+                    if (e.target.getAttribute('class') == 'enemy') {
+                        if (this.player.magazine > 0) {
+                            let parent = document.getElementById('stage')
+                            parent.removeChild(e.target)
+                            let targetIndex = this.enemyList.indexOf(this.enemyList.find(f =>
+                                f.enemyTag === e.target.getAttribute('id')
+                            ))
+                            clearTimeout(this.enemyList.find(f =>
+                                f.enemyTag === e.target.getAttribute('id')
+                            ).attackTimer)
+                            this.enemyList.splice(targetIndex, 1)
+                        }
                     }
+                    this.player.shot(e.target)
                 }
-                this.player.shot(e.target)
-            }
-        })
+            })
 
-        window.addEventListener('keydown', e => {
-            if (e.key == 'r') {
-                this.player.reload();
-            }
-        })
-    }
+            window.addEventListener('keydown', e => {
+                if (e.key == 'r') {
+                    this.player.reload();
+                }
+            })
+        }
 
     this.gameOver = () => {
 
-    }
+        }
 
-    // Checks if:
-    // 1. Stops music,attack & enemy timers OK
-    // 2. Creates new DIV showing the screen "continue-screen" OK
-    // 4. In that screen, when player clicks in "Continue"...
-    //      5. Level goes up to the next number (+1)
-    //      6. Enemies array will be cleared and will dissapear from DOM
-    //      7. Changes background 
-    //      8. We call to a new CodeSniper game
-    //this.endOfStage = () => {
-    //    self.level.stage.bgm.pause()
-    //    clearInterval(this.enemyInterval)
-    //    this.enemyList.forEach(enemy => {
-    //        clearInterval(enemy.attackTimer)
-    //    });
-
-    //    const oldStage = document.getElementById('screen')
-    //    let newScreen = document.createElement('div')
-    //    newScreen.classList.add('stage')
-    //    newScreen.id = `continue-screen`
-    //    oldStage.appendChild(newScreen)
-
-    //    const stages = document.getElementById('stage')
-    //    stages.removeChild('div')  // Eliminar enemigos del DOM?
-
-    //    window.addEventListener('click', e => {
-    //        if (e.target.getAttribute('id') === 'continue') {
-    //            this.newStage()
-    //        }
-    //    })
-
-    //}
-
-    // Prepares and clears everything, so we can build after the new stage.
-    this.newStage = () => {
-        this.stage = stages.filter(e => { return e.level === level })[1]
-        this.enemyList = []
-
-
-
-    }
 }
-
 
 //Starts the game
 window.addEventListener('click', e => {
---
+++ script/maps.js
@@ -0,0 +1,47 @@
+const stages = [{
+    level: 1,
+    enemyRate: [1000, 5000], //1 new enemy between 1 and 5 seconds (random)
+    timer: 3000, //level timer is 1 minute
+    cleared: false,
+    class: 'level-one',
+    maplimits: [190, 320, 0, 475],
+    bgm: new Audio('media/sound/stage-music-1.mp3')
+},
+{
+    level: 2,
+    enemyRate: [500, 4500],
+    timer: 3000,
+    cleared: false,
+    class: 'level-two',
+    maplimits: [150, 320, 0, 475],
+    bgm: new Audio('media/sound/stage-music-2.mp3')
+},
+{
+    level: 3,
+    enemyRate: [500, 3500],
+    timer: 3000,
+    cleared: false,
+    class: 'level-three',
+    maplimits: [190, 320, 0, 475],
+    bgm: new Audio('media/sound/stage-music-3.mp3')
+},
+
+{
+    level: 4,
+    enemyRate: [500, 2500],
+    timer: 3000,
+    cleared: false,
+    class: 'level-four',
+    maplimits: [190, 320, 0, 475],
+    bgm: new Audio('media/sound/stage-music-3.mp3')
+},
+
+{
+    level: 5,
+    enemyRate: [500, 1000],
+    timer: 3000,
+    cleared: false,
+    class: 'level-five',
+    maplimits: [190, 320, 0, 475],
+    bgm: new Audio('media/sound/stage-music-3.mp3')
+}]
